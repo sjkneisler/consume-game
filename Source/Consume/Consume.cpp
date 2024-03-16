4a78d487fb933798eb78aeaa42ee7579347c9eb4
Steven Kneisler <sjkneisler@sep.com>
2024-03-16 14:00:25 -0400

    Split off SJK test map and add consume functionality

---
+++ Source/Consume/Consume.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Consume.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Consume, "Consume" );
